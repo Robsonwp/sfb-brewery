ab0209362e4c60719ff96814a6909bbb961d8971
Filip Perz <perzfilip@gmail.com>
2019-05-28 13:44:00 +0200

    Initial commit

---
+++ src/main/java/com/filipperz/brewery/SfbBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.filipperz.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfbBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfbBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filipperz/brewery/SfbBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipperz.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SfbBreweryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
